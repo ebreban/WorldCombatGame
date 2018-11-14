8d1f37df0ad49db16bba1527d2cb0c65192fe237
ebreban <42680176+ebreban@users.noreply.github.com>
2018-11-14 10:36:46 -0500

    updated fighter models

--- HighDamageFighter.py
+++ HighDamageFighter.py
@@ -1,1 +1,9 @@
 class HighDamageFighter(Hero , pygame.sprite.Sprite):
+
+    def __init__(self, name, health, damage):
+        self.health = 200
+        self.damage = 50
+        self.restTime = 3
+    def damage(self):
+    	return self.damage
+
--- HighHealthFighter.py
+++ HighHealthFighter.py
@@ -1,1 +1,9 @@
 class HighHealthFighter(Hero, pygame.sprite.Sprite):
+
+    def __init__(self, name):
+    	self.name = name
+        self.health = 300
+        self.damage = 25
+        self.restTime = 1 
+    def damage(self):
+    	return self.damage
